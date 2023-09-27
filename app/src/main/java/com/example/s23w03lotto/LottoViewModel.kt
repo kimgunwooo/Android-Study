7886d043b5c12a6018cc362fd792601427686636
김건우 <kwforu123@naver.com>
2023-09-27 15:37:40 +0900

    [FIX] ViewModel 변경 - LiveData 사용

--- app/src/main/java/com/example/s23w03lotto/LottoViewModel.kt
+++ app/src/main/java/com/example/s23w03lotto/LottoViewModel.kt
@@ -1,31 +1,36 @@
 package com.example.s23w03lotto
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class LottoViewModel : ViewModel( ) {
-    //private var _numbers = intArrayOf(0, 0, 0, 0, 0, 0)
-    private var _numbers = IntArray(6) { 0 }
-    val numbers
+    private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+    val numbers : LiveData<IntArray>
         get() = _numbers //copy해서 넘겨줌 (getter)
     fun generate() { //setter 역할
         //_numbers 초기화
-        _numbers = IntArray(6) { 0 }
-
+        //_numbers = IntArray(6) { 0 }
+        val newNumbers = IntArray(6) { 0 }
         var num = 0
-        for (i in _numbers.indices) { //indices = 모든 인덱스 집합
+        for (i in newNumbers.indices) { //indices = 모든 인덱스 집합
 //            _numbers[i] = Random.nextInt(1, 46)
             // 중복 검사
             do {
                 num = Random.nextInt(1, 46)
-            } while (_numbers.contains(num))
-            _numbers[i] = num
+            } while (newNumbers.contains(num))
+            newNumbers[i] = num
 
-            Log.i("Number!!!", _numbers[i].toString())
+            //Log.i("Number!!!", _numbers[i].toString())
         }
 
         // 정렬
-        _numbers.sort()
+        newNumbers.sort()
+
+        //newNumbers라는 새로운 객체를 만들어서 작업을 수행.
+        //이제 _numbers.value에 대입
+        _numbers.value = newNumbers
     }
 }
