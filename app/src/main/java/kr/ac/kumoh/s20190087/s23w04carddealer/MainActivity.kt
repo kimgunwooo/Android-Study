ae9b4b7af09519c3759fc48590aefe832d4f661f
김건우 <kwforu123@naver.com>
2023-09-27 16:26:35 +0900

    [ADD] 이미지 띄우기 - MainActivity에서 이미지 설정

--- app/src/main/java/kr/ac/kumoh/s20190087/s23w04carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190087/s23w04carddealer/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.ac.kumoh.s20190087.s23w04carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.s20190087.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+        main.card1.setImageResource(R.drawable.c_10_of_hearts)
     }
 }
