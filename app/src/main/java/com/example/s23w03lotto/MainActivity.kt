09f1752108fa4e6c36ced09ba4514f1424653ca6
김건우 <kwforu123@naver.com>
2023-09-27 15:38:01 +0900

    [FIX] MainActivity 변경 - Observer 사용

--- app/src/main/java/com/example/s23w03lotto/MainActivity.kt
+++ app/src/main/java/com/example/s23w03lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.s23w03lotto.LottoViewModel
 import com.example.s23w03lotto.databinding.ActivityMainBinding
@@ -30,21 +31,27 @@ class MainActivity : AppCompatActivity() {
 //        txtNum[5] = main.num6
 
         model = ViewModelProvider(this)[LottoViewModel::class.java] //::class.java 를 쓰면 java 언어에서도 동작되게 함.
-        txtNum.forEachIndexed { index, textView -> //Index가 필요업으면 forEach만 사용
-            textView?.text = model.numbers[index].toString() //nullable ?붙이기
-        }
+        setNumbersText();
+
+        model.numbers.observe(this, Observer { //Observer를 통해 값이 바뀌는걸 확인하고 값 넣기
+            setNumbersText()
+        })
 
         main.btnGenerate.setOnClickListener {
-            model.generate()
-            txtNum.forEachIndexed { index, textView ->
-                textView?.text = model.numbers[index].toString()
-            }
+            model.generate() //번호를 생성하는 일만 함.
+            //setNumbersText(); //값 출력
 //            main.num1.text = model.numbers[0].toString()
 //            main.num2.text = model.numbers[1].toString()
 //            main.num3.text = model.numbers[2].toString()
 //            main.num4.text = model.numbers[3].toString()
 //            main.num5.text = model.numbers[4].toString()
 //            main.num6.text = model.numbers[5].toString()
+        }
+    }
+
+    private fun setNumbersText(){
+        txtNum.forEachIndexed { index, textView -> //Index가 필요업으면 forEach만 사용
+            textView?.text = model.numbers.value!![index].toString() //nullable ?붙이기, !! - 무조건 값이 있다.
         }
     }
 
