666aef3161e0e7830ff3f44b0f751bfaebb7a39e
김건우 <kwforu123@naver.com>
2023-09-27 16:00:13 +0900

    [Init] project

---
+++ app/src/main/java/kr/ac/kumoh/s20190087/s23w04carddealer/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20190087.s23w04carddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
