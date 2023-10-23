6ae915fc07abadb800c6cf4091781e082137e9c2
김건우 <kwforu123@naver.com>
2023-10-23 15:34:26 +0900

    [FIX] OnClickListener 구현 - OnClick 함수 사용

--- app/src/main/java/kr/ac/kumoh/s20190087/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190087/s23w07intentdata/MainActivity.kt
@@ -3,10 +3,12 @@ package kr.ac.kumoh.s20190087.s23w07intentdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import kr.ac.kumoh.s20190087.s23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object{
         const val KEY_NAME = "location"
         const val MOUNTAIN = "mountain" //const 붙이면 시작 될 때 넣어버림 (고정) readOnly는 같음
@@ -20,17 +22,27 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.btnMountain.setOnClickListener {
-            Toast.makeText(this,"산 눌림", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME,MOUNTAIN)// key에 해당하는 부분은 string 으로, HashMap 형태로 넘겨줘야 함.
-            startActivity(intent)
+        main.btnMountain.setOnClickListener (this)
+        main.btnSea.setOnClickListener (this)
+    }
+
+    override fun onClick(v: View?) {
+
+        val intent = Intent(this, ImageActivity::class.java)
+        val value = when (v?.id) {
+            main.btnMountain.id -> {
+                Toast.makeText(this,"산 눌림", Toast.LENGTH_SHORT).show()
+                MOUNTAIN
+            }
+            main.btnSea.id -> {
+                Toast.makeText(this,"바다 눌림", Toast.LENGTH_SHORT).show()
+                SEA
+            }
+            else -> {
+                return
+            }
         }
-        main.btnSea.setOnClickListener {
-            Toast.makeText(this,"바다 눌림", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME,SEA)
-            startActivity(intent)
-        }
+        intent.putExtra(KEY_NAME,value)
+        startActivity(intent)
     }
 }
