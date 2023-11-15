ee2cf8919103ebf503219d47e104ec9393a52c79
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-11-15 15:36:31 +0900

    remember, mutableStateOf 를 통해 Clicker 학습

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.s23w1102counter"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.s23w1102counter"
--- app/src/main/java/com/example/s23w1102counter/MainActivity.kt
+++ app/src/main/java/com/example/s23w1102counter/MainActivity.kt
@@ -3,44 +3,76 @@ package com.example.s23w1102counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.s23w1102counter.ui.theme.S23W1102CounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            S23W1102CounterTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Clicker()
             }
+        }
+    }
+
+    
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit ){ //Composable 함수는 받는데, 반환 값은 없다. void = Unit
+    S23W1102CounterTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Clicker() {
+    //var txtString = "눌러주세요"; //기존 처럼 String을 사용하면 안됨
+    //var txtString by remember { mutableStateOf("눌러주세요.") } //상태를 사용하도록 변수를 변경
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S23W1102CounterTheme {
-        Greeting("Android")
+    val (txtString, setTxtString) = remember { mutableStateOf("눌러주세요")} //react 방식으로도 표현 가능
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = txtString,
+            fontSize = 70.sp,
+        )
+        Button(modifier = Modifier
+            .fillMaxWidth(),
+            onClick = {
+                //txtString = "눌렸습니다" //이렇게 해도 변하지 않음. 기존엔 id 값으로 접근
+                setTxtString("눌렸습니다") //react 방식
+            }) {
+            // Text(text = "눌러봐")
+            Text("눌러봐")
+        }
     }
 }
