0b5175fd11aff8342c3268cb45d4403103c59784
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-11-20 15:48:48 +0900

    ColumnScope 사용 & weight 적용

--- app/src/main/java/com/example/s23w12carddealer/MainActivity.kt
+++ app/src/main/java/com/example/s23w12carddealer/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -37,18 +40,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CardImages() {
-    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) { // 가로인지 확인
-        Text("가로")
+fun ColumnScope.CardImages() { // ColumnScope 를 사용해서 onCreate의 Column을 scope 함
+    if (LocalConfiguration.current.orientation
+        == Configuration.ORIENTATION_LANDSCAPE) {
+        // weight(1f)은 onCreate()에 있는 Column에 적용됨
+        // 버튼을 맨 밑에 위치시키기 위함
+        Row(modifier = Modifier.weight(1f)) {
+            Text("가로")
+        }
     }
     else {
-        Text("세로")
+        // 첫 번째 parameter가 modifier이므로 "modifier =" 안써도 됨
+        Column(Modifier.weight(1f)) {
+            // Row의 weight는 세로 화면에서 균등 분배
+            Row(Modifier.weight(1f)) {
+                Text("세로")
+            }
+            Row(Modifier.weight(1f)) {
+                Text("세로")
+            }
+            Row(Modifier.weight(1f)) {
+                Text("세로")
+            }
+        }
     }
 }
 
 @Composable
 fun ShuffleButton() {
-    Button(onClick = {}) {
+    Button(modifier = Modifier.fillMaxWidth(), onClick = {}) {
         Text("Good Luck")
     }
 }
